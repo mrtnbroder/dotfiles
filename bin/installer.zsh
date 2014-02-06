7aa609a17d632a8cc248ca90377d6fa98fe585ad
--get <mbroder@3pc.de>
2014-02-06 12:22:04 +0100

    updated prezto, some shellcheck.net fixes

--- bin/installer.zsh
+++ bin/installer.zsh
@@ -10,7 +10,7 @@ emulate -LR zsh # http://stackoverflow.com/questions/3964068/zsh-automatically-r
 
 setopt EXTENDED_GLOB
 
-printf "$(tput setaf 6)"
+printf "%s" "$(tput setaf 6)"
 printf "%s\n" '                _      _'
 printf "%s\n" '    |          | | o  | |'
 printf "%s\n" '  __|   __ _|_ | |    | |  _   ,'
@@ -18,20 +18,20 @@ printf "%s\n" ' /  |  /  \_|  |/  |  |/  |/  / \_'
 printf "%s\n" 'o\_/|_/\__/ |_/|__/|_/|__/|__/ \/'
 printf "%s\n" '               |\'
 printf "%s\n" '               |/ by Martin Broder'
-printf "$(tput sgr0)\n"
+printf "%s" "$(tput sgr0)\n"
 
 # Variables
 
-base_dir="${ZDOTDIR:-$HOME}/.dotfiles_v2"
+export base_dir="${ZDOTDIR:-$HOME}/.dotfiles_v2"
 # base_dir="${ZDOTDIR:-$HOME}/Dropbox/dotfiles"
-base_git="https://github.com/mrtnbroder/dotfiles_v2"
-base_tar="https://github.com/mrtnbroder/dotfiles_v2/tarball/master"
+export base_git="https://github.com/mrtnbroder/dotfiles_v2"
+export base_tar="https://github.com/mrtnbroder/dotfiles_v2/tarball/master"
 
-no_apps=true
-no_sync=false
-no_npm=false
-no_brew=false
-no_gem=false
+export no_apps=true
+export no_sync=false
+export no_npm=false
+export no_brew=false
+export no_gem=false
 
 # Downloading
 
@@ -39,18 +39,18 @@ if [[ ! -d $base_dir ]]; then
     printf "$(tput setaf 6)➜ %s$(tput sgr0)\n" "Downloading dotfiles"
 
     # create directory
-    mkdir $base_dir
+    mkdir "$base_dir"
     # # Get the tarball
-    curl -fsSLo ${ZDOTDIR:-$HOME}/dotfiles.tar.gz $base_tar
+    curl -fsSLo "${ZDOTDIR:-$HOME}"/dotfiles.tar.gz "$base_tar"
     # # Extract to the dotfiles directory
-    tar -zxf ${ZDOTDIR:-$HOME}/dotfiles.tar.gz --strip-components 1 -C $base_dir
+    tar -zxf "${ZDOTDIR:-$HOME}"/dotfiles.tar.gz --strip-components 1 -C "$base_dir"
     # # Remove the tarball
-    rm -rf ${ZDOTDIR:-$HOME}/dotfiles.tar.gz
+    rm -rf "${ZDOTDIR:-$HOME}"/dotfiles.tar.gz
 
     [ $? ] && echo "$(tput setaf 2)✔ Done.$(tput sgr0)"
 fi
 
-cd $base_dir
+cd "$base_dir"
 
 # Imports
 
@@ -60,10 +60,6 @@ source "lib/utils"
 source "lib/checks"
 
 source "lib/git"
-
-source "lib/brew"
-source "lib/npm"
-source "lib/gem"
 
 # initialise the git repository
 init_git
@@ -81,14 +77,18 @@ if is_confirmed; then
     create_gitconfig
 
     # run brew
+    source "lib/brew"
     init_brew
 
     # run npm
+    source "lib/npm"
     init_npm
 
-    # run gem
+    # link .gemrc
     link_gem
 
+    # run gem
+    source "lib/gem"
     init_gem
 
     # link prezto
